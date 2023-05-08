7d9291449fd4130bc2865da3cabbe3cbd7b2979e
Artemii Dovzhenko <rockenny@ymail.com>
2023-05-08 23:22:12 +0300

    HW_5

--- src/playlist.py
+++ src/playlist.py
@@ -7,38 +7,35 @@ from googleapiclient.discovery import build
 
 from src.video import Video
 
+
 class PlayList:
     youtube = None
 
     def __init__(self, playlist_id: str):
-        self.playlist_id = playlist_id
+        self._playlist_id = playlist_id
         PlayList.youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
-        self.playlist_data = PlayList.youtube.playlistItems().list(playlistId=self.playlist_id,
-                                                                   part='contentDetails,snippet',
-                                                                   maxResults=50,
-                                                                   ).execute()
-        self.video_ids: list[str] = [item['contentDetails']['videoId'] for item in self.playlist_data['items']]
+        self._playlist_data = PlayList.youtube.playlistItems().list(playlistId=self._playlist_id,
+                                                                    part='contentDetails,snippet',
+                                                                    maxResults=50,
+                                                                    ).execute()
+        self._video_ids: list[str] = [item['contentDetails']['videoId'] for item in self._playlist_data['items']]
 
-
-        self.channel_id = self.playlist_data['items'][0]['snippet']['channelId']
-        self.outer_playlist_data = self.youtube.playlists().list(channelId=self.channel_id,
-                                     part='contentDetails,snippet,player',
-                                     maxResults=50,
-                                     ).execute()
-        for item in self.outer_playlist_data['items']:
-            if item['id'] == self.playlist_id:
+        self._channel_id = self._playlist_data['items'][0]['snippet']['channelId']
+        self._outer_playlist_data = self.youtube.playlists().list(channelId=self._channel_id,
+                                                                  part='contentDetails,snippet,player',
+                                                                  maxResults=50,
+                                                                  ).execute()
+        for item in self._outer_playlist_data['items']:
+            if item['id'] == self._playlist_id:
                 self.title = item['snippet']['title']
 
-
-        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
-
-        #self.title = pl.outer_playlist_data['items']
+        self.url = f'https://www.youtube.com/playlist?list={self._playlist_id}'
 
     @property
     def total_duration(self):
         video_response = PlayList.youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(self.video_ids)
-                                               ).execute()
+                                                        id=','.join(self._video_ids)
+                                                        ).execute()
         all_duration = None
         for video in video_response['items']:
             iso_8601_duration = video['contentDetails']['duration']
@@ -50,18 +47,5 @@ class PlayList:
         return all_duration
 
     def show_best_video(self):
-        all_stats = [Video(video_id) for video_id in self.video_ids]
+        all_stats = [Video(video_id) for video_id in self._video_ids]
         return max(all_stats, key=lambda x: x.likes).video_url
-
-if __name__ == '__main__':
-    pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-    # pprint.pprint(pl.outer_playlist_data)
-    #pprint.pprint(pl.playlist_data)
-    print(pl.title)
-    print(pl.url)
-    print(pl.video_ids)
-    # pprint.pprint(str(pl.total_duration))
-    # pprint.pprint(pl.playlist_data)
-    print(pl.show_best_video())
-
-    # pprint.pprint(pl.playlist_data['items'][0]['snippet']['channelId'])
